8aeb5a7d4d0c6e4678afa1a5de5a3d1c79ab51d8
Eric George <ericgeorge@Erics-MacBook-Air-2.local>
2025-09-26 23:48:57 -0400

    basic stuff

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
 import Image from "next/image";
+import { Button } from "@/components/ui/button";
+
 
 export default function Home() {
     return (
-        <div>
-            <div className="text-3xl text-center">Vega Base</div>
+        <div className="flex flex-col items-center gap-3">
+            <div className="text-3xl text-center font-bold">Vega Base</div>
+            <Button variant={"outline"}>This is a button!</Button>
         </div>
     );
 }
